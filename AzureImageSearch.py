1739364a64b1b69d943483cd3194ba2f9671d052
JonathanLoungani <47465251+JonathanLoungani@users.noreply.github.com>
2019-02-09 13:13:37 -0500

    Update AzureImageSearch.py

--- AzureImageSearch.py
+++ AzureImageSearch.py
@@ -1,3 +1,4 @@
+# pip install azure-cognitiveservices-search-imagesearch
 from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
 from msrest.authentication import CognitiveServicesCredentials
 
@@ -7,33 +8,29 @@ import requests
 from io import BytesIO
 
 import tkinter as tk
-import time
 
+def openImage(search_term, time_span=2000):
+    subscription_key = "768fb70aaef144a4bbdcd344c8b80c8a"
 
-subscription_key = "768fb70aaef144a4bbdcd344c8b80c8a"
-search_term = "hamburger"
+    # Search using Azure Bing Image Search
+    client = ImageSearchAPI(CognitiveServicesCredentials(subscription_key))
+    image_results = client.images.search(query=search_term)
 
-client = ImageSearchAPI(CognitiveServicesCredentials(subscription_key))
-image_results = client.images.search(query=search_term)
+    if image_results.value: # Check if result found
+        first_image_result = image_results.value[0]
 
-if image_results.value:
-    first_image_result = image_results.value[0]
-    print("Total number of images returned: {}".format(len(image_results.value)))
-    print("First image thumbnail url: {}".format(first_image_result.thumbnail_url))
-    print("First image content url: {}".format(first_image_result.content_url))
+        # Display using Pillow
+        response = requests.get(first_image_result.thumbnail_url)
+        img = Image.open(BytesIO(response.content))
 
-    # Display using Pillow
-    response = requests.get(first_image_result.thumbnail_url)
-    img = Image.open(BytesIO(response.content))
+        # Open Image using tkinter
+        root = tk.Tk()
+        tkimage = ImageTk.PhotoImage(img)
+        tk.Label(root, image=tkimage).pack()
+        root.after(time_span, lambda: root.destroy())  # Destroy the widget after time_span ms
+        root.mainloop()
+    else:
+        print("No image results returned!")
 
-    root = tk.Tk()
-    tkimage = ImageTk.PhotoImage(img)
-    tk.Label(root, image=tkimage).pack()
-    root.mainloop()
-    time.sleep(5)
-    root.destroy()
-    # hide image
-
-
-else:
-    print("No image results returned!")
+# Sample Usage
+# openImage("Swimming", 1800)
